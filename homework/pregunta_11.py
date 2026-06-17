5643d6cd9b3130c4d9ca5d9727ec1a810407f0d0
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 13:00:07 -0500

    pregunta_11.py

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -1,10 +1,4 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
-
+import pandas as pd
 
 def pregunta_11():
     """
@@ -12,13 +6,24 @@ def pregunta_11():
     los valores de la columna `c4` del archivo `tbl1.tsv`.
 
     Rta/
-         c0       c4
-    0     0    b,f,g
-    1     1    a,c,f
-    2     2  a,c,e,f
-    3     3      a,b
+         c0      c4
+    0     0   b,f,g
+    1     1   a,c,f
+    2     2 a,c,e,f
+    3     3     a,b
     ...
-    37   37  a,c,e,f
-    38   38      d,e
-    39   39    a,d,f
+    37   37 a,c,e,f
+    38   38     d,e
+    39   39   a,d,f
     """
+    # Cargamos el archivo tbl1.tsv
+    df = pd.read_csv("files/input/tbl1.tsv", sep="\t")
+    
+    # 1. Ordenamos por c0 y c4 para que, al agrupar, las letras queden en orden
+    df_ordenado = df.sort_values(["c0", "c4"])
+    
+    # 2. Agrupamos por c0, seleccionamos c4 y unimos con ','
+    # .reset_index() se usa para devolver c0 a ser una columna normal
+    resultado = df_ordenado.groupby("c0")["c4"].apply(lambda x: ",".join(x)).reset_index()
+    
+    return resultado
