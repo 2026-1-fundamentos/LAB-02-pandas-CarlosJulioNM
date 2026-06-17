bba69ec263339728f444ccb61e1d3fecea9b3a94
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 12:40:29 -0500

    pregunta_05.py

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -1,9 +1,4 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
+import pandas as pd
 
 
 def pregunta_05():
@@ -20,3 +15,10 @@ def pregunta_05():
     E    9
     Name: c2, dtype: int64
     """
+    # Cargamos el DataFrame
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    
+    # Agrupamos por 'c1', seleccionamos 'c2' y calculamos el máximo
+    maximo = df.groupby("c1")["c2"].max()
+    
+    return maximo
