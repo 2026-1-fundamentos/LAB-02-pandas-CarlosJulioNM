6aa314ab4669bebe00959ef904fdb519335b3f85
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 13:01:09 -0500

    pregunta_13.py

--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -1,10 +1,5 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
 
+import pandas as pd
 
 def pregunta_13():
     """
@@ -20,3 +15,14 @@ def pregunta_13():
     E    275
     Name: c5b, dtype: int64
     """
+    # Cargamos los archivos
+    df0 = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    df2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    
+    # Realizamos un merge (unión) usando 'c0' como clave
+    merged_df = pd.merge(df0, df2, on="c0")
+    
+    # Agrupamos por 'c1', seleccionamos 'c5b' y sumamos
+    resultado = merged_df.groupby("c1")["c5b"].sum()
+    
+    return resultado
