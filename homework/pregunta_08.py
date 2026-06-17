aeee7e604f135a0b62d4741028a5597d1a06ebd0
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 12:57:25 -0500

    pregunta_08.py

--- homework/pregunta_08.py
+++ homework/pregunta_08.py
@@ -1,10 +1,4 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
-
+import pandas as pd
 
 def pregunta_08():
     """
@@ -12,13 +6,20 @@ def pregunta_08():
     data frame que contiene el archivo `tbl0.tsv`.
 
     Rta/
-         c0  c1   c2          c3  suma
-    0     0   E    1  1999-02-28     1
-    1     1   A    2  1999-10-28     3
-    2     2   B    5  1998-05-02     7
+          c0  c1  c2          c3  suma
+    0     0   E   1  1999-02-28     1
+    1     1   A   2  1999-10-28     3
+    2     2   B   5  1998-05-02     7
     ...
-    37   37   C    9  1997-07-22    46
-    38   38   E    1  1999-09-28    39
-    39   39   E    5  1998-01-26    44
+    37   37   C   9  1997-07-22    46
+    38   38   E   1  1999-09-28    39
+    39   39   E   5  1998-01-26    44
 
     """
+    # Cargamos el archivo tbl0.tsv
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    
+    # Creamos la nueva columna 'suma' sumando 'c0' y 'c2'
+    df["suma"] = df["c0"] + df["c2"]
+    
+    return df
