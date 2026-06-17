7d8b810a7ac81ff3cae78a0f32b31fec2fe6ad31
CarlosJulioNM <cnietomo@unal.edu.co>
2026-06-17 13:00:38 -0500

    pregunta_12.py

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -1,10 +1,4 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
-
+import pandas as pd
 
 def pregunta_12():
     """
@@ -13,12 +7,25 @@ def pregunta_12():
     tabla `tbl2.tsv`.
 
     Rta/
-         c0                                   c5
+         c0                           c5
     0     0        bbb:0,ddd:9,ggg:8,hhh:2,jjj:3
-    1     1              aaa:3,ccc:2,ddd:0,hhh:9
-    2     2              ccc:6,ddd:2,ggg:5,jjj:1
+    1     1        aaa:3,ccc:2,ddd:0,hhh:9
+    2     2        ccc:6,ddd:2,ggg:5,jjj:1
     ...
-    37   37                    eee:0,fff:2,hhh:6
-    38   38                    eee:0,fff:9,iii:2
-    39   39                    ggg:3,hhh:8,jjj:5
+    37   37                      eee:0,fff:2,hhh:6
+    38   38                      eee:0,fff:9,iii:2
+    39   39                      ggg:3,hhh:8,jjj:5
     """
+    # Cargamos el archivo tbl2.tsv
+    df = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    
+    # 1. Creamos la columna combinada 'c5' usando c5a y c5b
+    df["c5"] = df["c5a"] + ":" + df["c5b"].astype(str)
+    
+    # 2. Ordenamos por c0 y c5a para que al agrupar las claves queden ordenadas
+    df_ordenado = df.sort_values(["c0", "c5a"])
+    
+    # 3. Agrupamos por c0 y unimos los valores de 'c5' con ','
+    resultado = df_ordenado.groupby("c0")["c5"].apply(lambda x: ",".join(x)).reset_index()
+    
+    return resultado
